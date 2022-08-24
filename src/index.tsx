f859d83d838187b23092641173e76cc789ec2555
pydjangodev <pydjangodev@outlook.com>
2022-08-25 03:01:49 +0800

    added google roboto font, and custom classname generator

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
+import "./CustomClassNameSetup";
 // import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
