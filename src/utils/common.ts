79b73e8e2b071fd0f117e76baf2178b6293a593d
Endo <endo_aki22@outlook.com>
2023-04-18 00:50:11 -0700

    added formatBytes util

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -11,3 +11,25 @@ export const formatMinuteToReadable = (minutes: number) => {
     return `${m}m`;
   }
 };
+
+export const formatBytes = (bytes: number, decimals: number = 2) => {
+  if (!+bytes) return "0 Bytes";
+
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = [
+    "Bytes",
+    "KiB",
+    "MiB",
+    "GiB",
+    "TiB",
+    "PiB",
+    "EiB",
+    "ZiB",
+    "YiB",
+  ];
+
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`;
+};
