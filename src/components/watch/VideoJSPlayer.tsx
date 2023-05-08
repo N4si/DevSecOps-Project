42aa7ffb47b8f8c79d5a95040798c7c311097820
Endo <endo_aki22@outlook.com>
2023-05-08 05:24:53 -0700

    fix VideoJSPlayer for code split, dynamic import

--- src/components/watch/VideoJSPlayer.tsx
+++ src/components/watch/VideoJSPlayer.tsx
@@ -1,8 +1,5 @@
 import { useEffect, useRef } from "react";
-import videojs from "video.js";
 import Player from "video.js/dist/types/player";
-
-import "video.js/dist/video-js.css";
 
 export default function VideoJSPlayer({
   options,
@@ -11,7 +8,7 @@ export default function VideoJSPlayer({
   options: any;
   onReady: (player: Player) => void;
 }) {
-  const videoRef = useRef<HTMLDivElement | null>(null);
+  const videoRef = useRef<HTMLDivElement>(null);
   const playerRef = useRef<Player | null>(null);
 
   useEffect(() => {
@@ -19,12 +16,23 @@ export default function VideoJSPlayer({
     if (!playerRef.current) {
       // The Video.js player needs to be _inside_ the component el for React 18 Strict Mode.
       const videoElement = document.createElement("video-js");
+      // videoElement.classList.add("vjs-big-play-centered", "vjs-16-9");
 
       videoRef.current?.appendChild(videoElement);
-
-      const player = (playerRef.current = videojs(videoElement, options, () => {
-        onReady && onReady(player);
-      }));
+      import("video.js").then(async ({ default: videojs }) => {
+        await import("video.js/dist/video-js.css");
+        if (options["techOrder"].includes("youtube")) {
+          // eslint-disable-next-line camelcase
+          await import("videojs-youtube");
+        }
+        const player = (playerRef.current = videojs(
+          videoElement,
+          options,
+          () => {
+            onReady && onReady(player);
+          }
+        ));
+      });
 
       // You could update an existing player in the `else` block here
       // on prop change, for example:
@@ -41,7 +49,7 @@ export default function VideoJSPlayer({
     const player = playerRef.current;
 
     return () => {
-      if (player) {
+      if (player && !player.isDisposed()) {
         player.dispose();
         playerRef.current = null;
       }
